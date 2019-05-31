07524b50f880f20b668d71bb1f3db018d7c77866
Toth-Gabor <upcgabor@gmail.com>
2019-05-31 15:15:34 +0200

    added userId field

--- src/main/java/com/codecool/web/model/Coupon.java
+++ src/main/java/com/codecool/web/model/Coupon.java
@@ -5,23 +5,28 @@ import java.util.Objects;
 public final class Coupon extends AbstractModel {
 
     private final String name;
+    private final int userId;
     private final int percentage;
-
-    public Coupon(int id, String name, int percentage) {
+    
+    public Coupon(int id, String name, int userId, int percentage) {
         super(id);
         this.name = name;
+        this.userId = userId;
         this.percentage = percentage;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
+    public int getUserId() {
+        return userId;
+    }
+    
     public int getPercentage() {
         return percentage;
     }
-
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
