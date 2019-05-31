23f6f908df59b574cf631cfbcd316e65f63ad92c
Toth-Gabor <upcgabor@gmail.com>
2019-05-31 15:26:43 +0200

    added user_id to addCoupon method

--- src/main/java/com/codecool/web/service/simple/SimpleCouponService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleCouponService.java
@@ -37,9 +37,9 @@ public final class SimpleCouponService implements CouponService {
     }
 
     @Override
-    public Coupon addCoupon(String name, String percentage) throws SQLException, ServiceException {
+    public Coupon addCoupon(String name, String userId, String percentage) throws SQLException, ServiceException {
         try {
-            return couponDao.add(name, Integer.parseInt(percentage));
+            return couponDao.add(name, Integer.parseInt(userId), Integer.parseInt(percentage));
         } catch (NumberFormatException ex) {
             throw new ServiceException("Percentage must be an integer");
         } catch (IllegalArgumentException ex) {
